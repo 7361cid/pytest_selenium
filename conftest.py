73e108d6642c8224a77138af0b6dd9c49034f111
Ilia <chernoffilya1997@mail.ru>
2025-04-20 11:16:42 +0700

    Allure Скрины

--- conftest.py
+++ conftest.py
@@ -1,3 +1,6 @@
+import datetime
+
+import allure
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -11,9 +14,9 @@ def driver():
     options.add_argument("--no-sandbox")
     options.add_argument('--ignore-ssl-errors')
     options.accept_insecure_certs = True
-   # capabilities = {"acceptInsecureCerts": True, }
     driver = webdriver.Chrome(options=options)
-    #webdriver.Chrome(chrome_options=options, service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
     yield driver
+    attach = driver.get_screenshot_as_png()
+    allure.attach(attach, name=f"Screenshot {datetime.datetime.now()}", attachment_type=allure.attachment_type.PNG)
     driver.quit()
